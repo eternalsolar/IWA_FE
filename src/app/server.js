90b7b671d7f68cba970791679a560ddb651f0940
Eternal Solar <eternalsolar0106@gmail.com>
2020-11-09 15:15:36 +0700

    fix

---
+++ src/app/server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var proxy = require('./../../proxy.conf');
+var app = express();
+
+app.use(express.static(__dirname + '/dist'));
+app.get('/assets', function(req,res){
+    res.sendFile(__dirname + 'dist/assets');
+})
+
+app.get('/*', function(req,res){
+    res.sendFile(__dirname + 'dist/index.html');
+})
+
+var port = process.env.PORT || 8080;
+app.listen(port, function(){
+    const proxyConfig = {
+        context: '/api',
+        pathRewrite: { '^/api': '' },
+        target: 'https://test-iwa.herokuapp.com/',
+        changeOrigin: true,
+        secure: true
+      }
+    proxy(proxyConfig);
+    console.log('server is running on port ' + port + '.');
+})
